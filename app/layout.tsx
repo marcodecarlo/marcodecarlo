4da8590dbcfe734035eb6fa108ffa0dda7b07d22
marcodecarlo <marco.decarlo3@gmail.com>
2024-09-14 15:40:53 +0200

    add background

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Exo_2 } from "next/font/google";
+
 import "@/styles/globals.css";
+import { BackgroundFilter } from "@/components";
 
 export const metadata: Metadata = {
   title: "Marco De Carlo",
@@ -16,9 +18,13 @@ children,
 }>) {
   return (
     <html lang="it">
-      <body className={`min-h-screen font-sans bg-gray-900 selection:text-white selection:bg-green-950 ${exo.variable}`}>
-      <div className="layout-sm relative z-10 grid gap-y-8 px-4 pt-12 text-green-200/90 xl:layout-xl xl:gap-x-9 xl:px-0 [&>*]:col-start-2 xl:[&>*]:col-start-3">
-          {children}
+      <body className={`overscroll-y-none font-sans bg-stone-900 selection:text-white selection:bg-green-950 ${exo.variable}`}>
+        <BackgroundFilter/>
+        <div className="layout-sm relative z-10 grid gap-y-8 px-4 pt-12 text-green-200/90 xl:layout-xl xl:gap-x-9 xl:px-0 [&>*]:col-start-2 xl:[&>*]:col-start-3">
+            {children}
+        </div>
+        <div className="pointer-events-none absolute inset-0 overflow-hidden">
+          <div className="h-full bg-[url('https://res.cloudinary.com/graziadecarlo/image/upload/v1726317965/bg_gradient.png')] bg-top bg-no-repeat opacity-[0.8]" ></div>
         </div>
       </body>
     </html>
---
+++ components/index.ts
@@ -0,0 +1,1 @@
+export { default as BackgroundFilter } from "./svg/BackgroundFilter";
