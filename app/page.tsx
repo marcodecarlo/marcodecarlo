d388e893dbf0f0b9d73dd57243cf7e4fc62843e2
marcodecarlo <marco.decarlo3@gmail.com>
2024-09-14 18:46:31 +0200

    fix layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import { ProfileImage } from "@/components"
 
 export default function Home() {
   return (
-    <div className="rounded-2xl bg-white/5 p-7 shadow-lg  shadow-green-800/50">
-      <div className="flex items-center space-x-6">
+    <div className="rounded-2xl bg-white/5 p-4 md:p-7 shadow-lg  shadow-green-800/50">
+       <div className="flex flex-wrap items-center">
+        
         <ProfileImage/>
-        <div className="mb-auto">
+   
+        <div className="mx-3 md:mx-6 flex-1">
           <h1 className="text-4xl font-semibold leading-none text-green-100">
             Marco De Carlo
           </h1>
@@ -14,6 +16,14 @@ export default function Home() {
             Web Developer
           </h2>
         </div>
+
+        <div className="my-5">
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+
+        </div>
+       
       </div>
     </div>
   );
--- components/common/ProfileImage.tsx
+++ components/common/ProfileImage.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function ProfileImage() {
   return (
-    <div className="group rounded-full bg-gradient-to-tl from-green-700/50 to-green-400/60 p-0.5 shadow-lg">
+    <div className="group rounded-full bg-gradient-to-tl from-green-700/50 to-green-400/60 p-0.5 shadow-lg mb-auto">
       <div className="h-36 w-36 rounded-full p-1 shadow-lg ring-8 ring-green-900/10 transition duration-300 group-hover:scale-105">
         <Image
           src="/marco_decarlo.png"
