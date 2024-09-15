807331f6db87f91db7d8c4a571591f2f3cf0cae8
marcodecarlo <marco.decarlo3@gmail.com>
2024-09-15 17:23:14 +0200

    add redirect to home

--- components/common/ProfileImage.tsx
+++ components/common/ProfileImage.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function ProfileImage() {
   return (
-    <div className="group rounded-full bg-gradient-to-tl from-green-700/50 to-green-400/60 p-0.5 shadow-lg mb-auto mx-auto">
+    <Link
+      href="/"
+      className="group rounded-full bg-gradient-to-tl from-green-700/50 to-green-400/60 p-0.5 shadow-lg mb-auto mx-auto"
+    >
       <div className="h-36 w-36 rounded-full p-1 shadow-lg ring-8 ring-green-900/10 transition duration-300 group-hover:scale-105">
         <Image
           src="/marco_decarlo.png"
@@ -14,6 +18,6 @@ export default function ProfileImage() {
           alt="Foto di Marco De Carlo"
         />
       </div>
-    </div>
+    </Link>
   );
 }
