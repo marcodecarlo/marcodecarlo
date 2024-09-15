c934d3578eda292bedcd4fa6da11e79f4ab049f9
marcodecarlo <marco.decarlo3@gmail.com>
2024-09-15 18:42:02 +0200

    add bio

--- app/page.tsx
+++ app/page.tsx
@@ -16,17 +16,36 @@ export default function Home() {
           <Nav />
         </div>
 
-        <div className="my-5">
+        <div className="my-5 space-y-3">
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+            Ciao, sono Marco, un Web Developer di 31 anni con base ad
+            Alberobello, ma sempre con la valigia pronta. La mia passione per
+            l'informatica è iniziata presto: ho frequentato le scuole superiori
+            come perito informatico e mi sono laureato in Informatica presso
+            l'Università degli Studi di Milano.
+          </p>
+          <p>
+            Da piccolo, passavo molto tempo a giocare con i Lego, costruendo e
+            inventando storie con i personaggi. Questo amore per la costruzione
+            e la creatività ha trovato terreno fertile nella programmazione, che
+            mi ha permesso di costruire e realizzare software innovativi e
+            funzionali.
+          </p>
+          <p>
+            Inizialmente mi sono approcciato al mondo del Frontend e all'uso di
+            programmi come Photoshop, Illustrator e After Effects. Con il tempo,
+            ho ampliato le mie conoscenze anche ai linguaggi di Backend,
+            permettendomi di creare E-Commerce, Blog e Siti Web per clienti in
+            cerca di consulenza.
+          </p>
+          <p>
+            Quando un progetto mi appassiona, mi dedico completamente, curando
+            ogni dettaglio e lavorando in team per apprendere nuove tecnologie e
+            ottimizzare il flusso di lavoro.
+          </p>
+          <p>
+            Sono sempre alla ricerca di nuove sfide e opportunità per mettere le
+            mani su progetti stimolanti e innovativi.
           </p>
         </div>
       </div>
