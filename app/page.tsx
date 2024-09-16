15f3916069062cb1c3fa2dc44f0da17bd9a408cc
marcodecarlo <marco.decarlo3@gmail.com>
2024-09-16 12:05:48 +0200

    fix navbar component and text bio

--- app/page.tsx
+++ app/page.tsx
@@ -18,34 +18,34 @@ export default function Home() {
 
         <div className="my-5 space-y-3">
           <p>
-            Ciao, sono Marco, un Web Developer di 31 anni con base ad
+            {`Ciao, sono Marco, un Web Developer di 31 anni con base ad
             Alberobello, ma sempre con la valigia pronta. La mia passione per
             l'informatica è iniziata presto: ho frequentato le scuole superiori
             come perito informatico e mi sono laureato in Informatica presso
-            l'Università degli Studi di Milano.
+            l'Università degli Studi di Milano.`}
           </p>
           <p>
-            Da piccolo, passavo molto tempo a giocare con i Lego, costruendo e
+            {`Da piccolo, passavo molto tempo a giocare con i Lego, costruendo e
             inventando storie con i personaggi. Questo amore per la costruzione
             e la creatività ha trovato terreno fertile nella programmazione, che
             mi ha permesso di costruire e realizzare software innovativi e
-            funzionali.
+            funzionali.`}
           </p>
           <p>
-            Inizialmente mi sono approcciato al mondo del Frontend e all'uso di
+            {`Inizialmente mi sono approcciato al mondo del Frontend e all'uso di
             programmi come Photoshop, Illustrator e After Effects. Con il tempo,
             ho ampliato le mie conoscenze anche ai linguaggi di Backend,
             permettendomi di creare E-Commerce, Blog e Siti Web per clienti in
-            cerca di consulenza.
+            cerca di consulenza.`}
           </p>
           <p>
-            Quando un progetto mi appassiona, mi dedico completamente, curando
+            {`Quando un progetto mi appassiona, mi dedico completamente, curando
             ogni dettaglio e lavorando in team per apprendere nuove tecnologie e
-            ottimizzare il flusso di lavoro.
+            ottimizzare il flusso di lavoro.`}
           </p>
           <p>
-            Sono sempre alla ricerca di nuove sfide e opportunità per mettere le
-            mani su progetti stimolanti e innovativi.
+            {`Sono sempre alla ricerca di nuove sfide e opportunità per mettere le
+            mani su progetti stimolanti e innovativi.`}
           </p>
         </div>
       </div>
