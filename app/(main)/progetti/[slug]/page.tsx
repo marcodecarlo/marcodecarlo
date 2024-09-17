81c72236e3f2e0aed7c40e81dd0583d78e416fd2
marcodecarlo <marco.decarlo3@gmail.com>
2024-09-17 17:27:01 +0200

    add metadata

--- app/(main)/progetti/[slug]/page.tsx
+++ app/(main)/progetti/[slug]/page.tsx
@@ -1,16 +1,36 @@
 import { allPosts } from "@/.contentlayer/generated";
 import { Post } from "@/components";
-
 import { formatPost } from "@/lib";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 export async function generateStaticParams() {
   const posts = allPosts
     .filter((p) => p.status === "published")
     .map((p) => ({ slug: p.slug }));
+
   return posts.map((post) => ({
     slug: post.slug,
   }));
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
+  const post = allPosts.find((post) => post.slug === params?.slug);
+
+  if (!post) {
+    notFound();
+  }
+
+  const url = `/progetti/${post.slug}`;
+
+  return {
+    title: `${post.title} ⋅ Marco De Carlo`,
+    alternates: { canonical: url },
+  };
 }
 
 export default function Page({ params }: { params: { slug: string } }) {
