f57dc7c0acc6eadf9fe7c0e0afe9ef715c31725e
marcodecarlo <marco.decarlo3@gmail.com>
2024-09-18 14:43:19 +0200

    fix bio

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/components";
+import Balancer from "react-wrap-balancer";
 
 export default function Home() {
   return (
@@ -7,34 +8,44 @@ export default function Home() {
 
       <div className="my-5 space-y-3">
         <p>
-          {`Ciao, sono Marco, un Web Developer di 31 anni con base ad
+          <Balancer>
+            {`Ciao, sono Marco, un Web Developer di 31 anni con base ad
             Alberobello, ma sempre con la valigia pronta. La mia passione per
             l'informatica è iniziata presto: ho frequentato le scuole superiori
             come perito informatico e mi sono laureato in Informatica presso
             l'Università degli Studi di Milano.`}
+          </Balancer>
         </p>
         <p>
-          {`Da piccolo, passavo molto tempo a giocare con i Lego, costruendo e
+          <Balancer>
+            {`Da piccolo, passavo molto tempo a giocare con i Lego, costruendo e
             inventando storie con i personaggi. Questo amore per la costruzione
             e la creatività ha trovato terreno fertile nella programmazione, che
             mi ha permesso di costruire e realizzare software innovativi e
             funzionali.`}
+          </Balancer>
         </p>
         <p>
-          {`Inizialmente mi sono approcciato al mondo del Frontend e all'uso di
+          <Balancer>
+            {`Inizialmente mi sono approcciato al mondo del Frontend e all'uso di
             programmi di grafica come Photoshop, Illustrator e After Effects. Con il tempo,
             ho ampliato le mie conoscenze anche ai linguaggi di Backend,
             permettendomi di creare E-Commerce, Blog e Siti Web per clienti in
             cerca di consulenza.`}
+          </Balancer>
         </p>
         <p>
-          {`Quando un progetto mi appassiona, mi dedico completamente, curando
+          <Balancer>
+            {`Quando un progetto mi appassiona, mi dedico completamente, curando
             ogni dettaglio e lavorando in team per apprendere nuove tecnologie e
             ottimizzare il flusso di lavoro.`}
+          </Balancer>
         </p>
         <p>
-          {`Sono sempre alla ricerca di nuove sfide e opportunità per mettere le
+          <Balancer>
+            {`Sono sempre alla ricerca di nuove sfide e opportunità per mettere le
             mani su progetti stimolanti e innovativi.`}
+          </Balancer>
         </p>
       </div>
     </div>
