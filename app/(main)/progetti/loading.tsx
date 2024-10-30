9566abb30591b5fdf4d4b6a7f2a58735253e7dac
marcodecarlo <marco.decarlo3@gmail.com>
2024-10-30 23:05:00 +0100

    add post skeleton

---
+++ app/(main)/progetti/loading.tsx
@@ -0,0 +1,11 @@
+import { PostPreviewSkeleton } from "@/components";
+
+export default function Loading() {
+  return (
+    <div className="mt-8 space-y-10">
+      {Array.from({ length: 3 }).map((item, index) => {
+        return <PostPreviewSkeleton key={index} />;
+      })}
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Exo_2 } from "next/font/google";
+import { Exo_2, Redacted_Script } from "next/font/google";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import { myMeta } from "@/lib";
 import {
@@ -20,6 +20,13 @@ const exo = Exo_2({
   variable: "--font-exo",
 });
 
+const redacted = Redacted_Script({
+  subsets: ["latin"],
+  weight: ["400"],
+  style: ["normal"],
+  variable: "--font-redacted",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -28,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="it">
       <body
-        className={`overscroll-y-none font-sans bg-stone-900 selection:text-white selection:bg-green-950 relative ${exo.variable}`}
+        className={`overscroll-y-none font-sans bg-stone-900 selection:text-white selection:bg-green-950 relative ${exo.variable} ${redacted.variable}`}
       >
         <BackgroundFilter />
         <div className="layout-sm relative z-10 grid gap-y-8 px-4 pt-12 text-green-100/90 xl:layout-xl xl:gap-x-9 xl:px-0 [&>*]:col-start-2 xl:[&>*]:col-start-3">
